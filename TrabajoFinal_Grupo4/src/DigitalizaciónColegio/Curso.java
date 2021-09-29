c068921553e3da5e87172e15875f6ec8b61b703c
CarlosRzUPC <u201624552@upc.edu.pe>
2021-09-29 00:21:59 -0500

    Merge remote-tracking branch 'origin/master'

--- TrabajoFinal_Grupo4/src/DigitalizaciónColegio/Curso.java
+++ TrabajoFinal_Grupo4/src/DigitalizaciónColegio/Curso.java
@@ -1,4 +1,20 @@
 package DigitalizaciónColegio;
 
-public class Curso {
-}
+    public abstract class Curso {
+
+        private String codigo;
+        private String nombre;
+        private String seccion;
+        private String coordinador;
+
+        public Curso(String codigo, String nombre, String seccion, String coordinador) {
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.seccion = seccion;
+            this.coordinador = coordinador;
+        }
+
+
+
+
+    }
---
+++ TrabajoFinal_Grupo4/src/Modelo_Alumno/Alumno.java
@@ -0,0 +1,25 @@
+package Modelo_Alumno;
+
+public class Alumno {
+    private String codigo;
+    private String nombre;
+    private String apellidos;
+    private String nombrePadre;
+    private String nombreMadre;
+
+    public Alumno() {
+    }
+
+    public Alumno(String codigo, String nombre, String apellidos, String nombrePadre, String nombreMadre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.nombrePadre = nombrePadre;
+        this.nombreMadre = nombreMadre;
+    }
+
+
+
+
+}
+
